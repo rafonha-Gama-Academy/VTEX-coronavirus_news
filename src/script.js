6f32cd78df2bce85d07efc7cfc516409200f02f6
rafonha <johndoe@example.comrafaela.sdp25@gmail.com>
2020-04-14 21:48:49 -0300

    fix read more button

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 function readMore() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+    var btnText = document.getElementById("readMore-btn");
   
     if (dots.style.display === "none") {
       dots.style.display = "inline";
